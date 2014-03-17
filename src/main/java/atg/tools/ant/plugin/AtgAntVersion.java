05e4833038c7c04cc3854f81d1f96bd62ebb7225
Matt Sicker <matthew.sicker@ahold.com>
2014-03-17 18:35:48 -0500

    Update version to 2.0 snapshot.

--- src/main/java/atg/tools/ant/plugin/AtgAntVersion.java
+++ src/main/java/atg/tools/ant/plugin/AtgAntVersion.java
@@ -3,7 +3,7 @@ package atg.tools.ant.plugin;
 public class AtgAntVersion {
 
     public static void main(String[] args) {
-        System.out.println("AtgAnt version 1.0.2-SNAPSHOT");
+        System.out.println("AtgAnt version 2.0-SNAPSHOT");
     }
 
 }
